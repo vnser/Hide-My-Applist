ecdf0683d89a00ebaf6b0787956e461150654fcd
vring <you@example.com>
2025-04-24 23:17:33 +0800

    111

--- common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
+++ common/src/main/java/icu/nullptr/hidemyapplist/common/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String APP_PACKAGE_NAME = "com.vring.hideapplist";
 
-    public static final String PROVIDER_AUTHORITY = "com.tsng.hidemyapplist.ServiceProvider";
+    public static final String PROVIDER_AUTHORITY = "com.vring.hideapplist.ServiceProvider";
     public static final String GMS_PACKAGE_NAME = "com.google.android.gms";
     public static final String GSF_PACKAGE_NAME = "com.google.android.gsf";
     public static final String UPDATE_URL_BASE = "https://api.nullptr.icu/android/hide-my-applist/static/";
